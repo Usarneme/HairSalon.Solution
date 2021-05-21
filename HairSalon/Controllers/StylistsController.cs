3a894078f29634f8ece159ca30ead21af6fbc91e
Usarneme <jamestcoop@gmail.com>
2021-05-21 10:03:21 -0700

    :lipstick: adjusts formatting and styles for stylists/index page

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -1,10 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
   public class StylistsController : Controller
   {
+
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/stylists")]
-    public ActionResult Index() { return View(); }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
   }
 }
