27530f16dc10a4f4b3331c105fdf163135fa187b
Usarneme <jamestcoop@gmail.com>
2021-05-21 12:03:24 -0700

    :building_construction: updates create view and controller for stylists and clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,10 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
 namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
+
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/clients")]
-    public ActionResult Index() { return View(); }
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+
+    [HttpGet("/clients/create")]
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
   }
 }
