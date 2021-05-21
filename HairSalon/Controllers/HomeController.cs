696e136debb50c22e7329d00a5b690c2102292c1
Usarneme <jamestcoop@gmail.com>
2021-05-21 15:56:38 -0700

    :white_check_mark: gets search functionality working, showing results on search view page

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,20 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 using System;
-using System.Web;
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly HairSalonContext _db;
+
+    public HomeController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index() { return View(); }
 
     [HttpGet("/Home/Search")]
     public ActionResult Search([FromQuery(Name = "Search")] string query)
     {
-      // TODO
-      Console.WriteLine("HIT SEARCH ROUTE; query: {0}",query);
-      Console.WriteLine(query);
+      ViewBag.Stylists = _db.Stylists.FirstOrDefault(stylist => stylist.Name == query);
+      ViewBag.Clients = _db.Clients.FirstOrDefault(client => client.Name == query);
       ViewBag.Query = query;
       return View();
     }
